6184e4f559006a5d639f54fb01b45df97a7c9f99
19alinka <91919141+19alinka@users.noreply.github.com>
2025-02-19 15:10:09 +0300

    Add files

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
+
+export default defineConfig({
+    root: './src',
+    publicDir: '../public',
+    build: {
+        outDir: '../dist',
+    },
+    base: "/sovcom",
+    plugins: [
+        ViteImageOptimizer({
+            webp: {
+                quality: 80,
+            },
+        }),
+    ],
+});
